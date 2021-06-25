60c549c6c7b4fb94f41e34eb2ed50f004de669ed
eXebyss <exeleroxz@gmail.com>
2021-06-25 22:38:16 +0300

    1st Save

--- src/App.js
+++ src/App.js
@@ -1,23 +1,169 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { checkButton, clickButton, selectState1, selectState2 } from "./index";
+import "./App.css";
 
 function App() {
+  const dispatch = useDispatch();
+  let state1 = useSelector(selectState1);
+  let state2 = useSelector(selectState2);
+  console.log(`Current state1 is: ${state1}`);
+  console.log(`Current state2 is: ${state2}`);
+  // const CLICKBUTTON = "buttons/clickButton";
+  // const CHECKBUTTON = "buttons/checkButton";
+
+  // const changeFirstState = () => {
+  //   return {
+  //     type: CHECKBUTTON,
+  //     payload: !state1,
+  //   };
+  // };
+
+  // const changeSecondState = () => {
+  //   return {
+  //     type: CLICKBUTTON,
+  //     payload: !state2,
+  //   };
+  // };
+
+  // const clickButton1 = () => {
+  //   dispatch(changeFirstState());
+  // };
+
+  // const clickButton2 = () => {
+  //   dispatch(changeSecondState());
+  // };
+
+  const clickButton1 = () => {
+    dispatch(checkButton(!state1));
+  };
+
+  const clickButton2 = () => {
+    dispatch(clickButton(!state2));
+  };
+
+  //==========
+  //Render 2nd Button
+
+  const renderButton2 = () => {
+    if (state1) {
+      return (
+        <div>
+          <div className="clickButton">
+            <button onClick={() => clickButton2()}>
+              2. Click me to render something.
+            </button>
+          </div>
+          <div>{state2 ? "Button is clicked." : "Button is not clicked."}</div>
+        </div>
+      );
+    }
+  };
+
+  //==========
+  //Cell content
+
+  const tableValueForTheRow = (i) => {
+    return (i *= 100);
+  };
+
+  const tableValueForTheFirstColumn = (j) => {
+    return (j *= 100);
+  };
+
+  //This one cell value is only for test
+  const cellContent = (i, j) => {
+    return (
+      tableValueForTheRow(i-1) + tableValueForTheFirstColumn(j)
+    )
+  };
+
+  //==========
+  //Render 1st row as a table head
+
+  const renderTableHead = (i) => {
+    return <th key={i}>{i === 0 ? "0" : tableValueForTheRow(i)}</th>;
+  };
+
+  const renderTableHeads = (n) => {
+    let tableHeads = [];
+    for (let i = 0; i < n; i++) {
+      tableHeads.push(renderTableHead(i));
+    }
+    return tableHeads;
+  };
+
+  const renderTableRowWithHeads = (i) => {
+    return <tr>{renderTableHeads(i)}</tr>;
+  };
+
+  //==========
+  //Render all other rows, where only 1st cell will have the same number
+  //as it is in the table head row
+
+  const renderTableData = (cellValue, j) => {
+    let i = cellValue;
+    return (
+      <td key={j === 1 ? i : i + j}>
+        {j === 1 ? tableValueForTheFirstColumn(i) : cellContent(i, j)}
+      </td>
+    );
+  };
+
+  const renderTableCells = (cellValue) => {
+    let tableCells = [];
+    for (let j = 1; j < 22; j++) {
+      tableCells.push(renderTableData(cellValue, j));
+    }
+    return tableCells;
+  };
+
+  // const renderTableCellsWithData = (cellValue) => {
+  //   return renderTableCells(cellValue);
+  // };
+
+  const renderTableRow = (cellValue) => {
+    return <tr key={cellValue}>{renderTableCells(cellValue)}</tr>;
+  };
+
+  const renderTableRows = (n) => {
+    let tableRows = [];
+    let a = [];
+    for (let i = 1; i < n; i++) {
+      let cellValue = a.push(i);
+      tableRows.push(renderTableRow(cellValue));
+    }
+    return tableRows;
+  };
+
+  const renderTableRowsWithData = (i) => {
+    return <tbody>{renderTableRows(i)}</tbody>;
+  };
+
+  //==========
+  //Render the whole table
+
+  const renderTable = () => {
+    if (state1 && state2) {
+      return (
+        <div>
+          <table>
+            <caption>Ducts</caption>
+            <thead>{renderTableRowWithHeads(21)}</thead>
+            {renderTableRowsWithData(21)}
+          </table>
+        </div>
+      );
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ fontSize: "2rem" }} className="App">
+      <button onClick={() => clickButton1()}>
+        {state1 ? "1. 2nd button is Active" : "1. 2nd button is NOT active"}
+      </button>
+      <div>{renderButton2()}</div>
+      <div>{renderTable()}</div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,67 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { configureStore, createSlice } from "@reduxjs/toolkit";
+import { Provider } from "react-redux";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// const initialState = {
+//   currentState1: false,
+//   currentState2: false,
+// };
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// const CLICKBUTTON = "CLICKBUTTON";
+// const CHECKBUTTON = "CHECKBUTTON";
+
+// const clickReducer = (state = initialState, action) => {
+//   switch (action.type) {
+//     case CHECKBUTTON:
+//       return { ...state, currentState1: action.payload };
+//     case CLICKBUTTON:
+//       return { ...state, currentState2: action.payload };
+//     default:
+//       return state;
+//   }
+// };
+
+const buttonSlice = createSlice({
+  name: "buttons",
+  initialState: {
+    currentState1: false,
+    currentState2: false,
+  },
+  reducers: {
+    checkButton: (state, action) => {
+      return { ...state, currentState1: action.payload };
+    },
+    clickButton: (state, action) => {
+      return { ...state, currentState2: action.payload };
+    },
+  },
+});
+
+const store = configureStore({
+  reducer: {
+    buttons: buttonSlice.reducer,
+  },
+});
+
+export const {checkButton, clickButton} = buttonSlice.actions;
+export const selectState1 = (state) => state.buttons.currentState1;
+export const selectState2 = (state) => state.buttons.currentState2;
+
+const root = document.getElementById("root");
+
+const render = () => {
+  ReactDOM.render(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+    root
+  );
+};
+
+render();
+//console.log(buttonSlice.actions.checkButton())
+// console.log(buttonSlice.actions.clickButton())
+//store.subscribe(() => console.log("Current state is: ", store.getState()));
