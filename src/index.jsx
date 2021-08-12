6398b0576594df17823e29be1e5608a9e9d113f2
eXebyss <mysticlord27@gmail.com>
2021-08-12 23:18:34 +0300

    sw test2

--- src/index.jsx
+++ src/index.jsx
@@ -4,6 +4,7 @@ import "./index.css";
 import App from "./App/App.jsx";
 import store from "./Store/store.js";
 import { Provider } from "react-redux";
+import "./serviceWorkerRegistration.js"
 
 const root = document.getElementById("root");
 
@@ -17,16 +18,3 @@ const render = () => {
 };
 
 render();
-
-if ("serviceWorker" in navigator) {
-  window.addEventListener("load", () => {
-    navigator.serviceWorker
-      .register("/service-worker.js")
-      .then((registration) => {
-        console.log("SW registered: ", registration);
-      })
-      .catch((registrationError) => {
-        console.log("SW registration failed: ", registrationError);
-      });
-  });
-}
