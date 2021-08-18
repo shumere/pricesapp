a65fdd72c3d0009908cf6988c35fdf24e4e4a657
eXebyss <mysticlord27@gmail.com>
2021-08-18 23:17:32 +0300

    upgrade webpack 4 to 5 + all dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,14 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const WebpackPwaManifest = require("webpack-pwa-manifest");
+const CopyPlugin = require("copy-webpack-plugin");
 const WorkboxPlugin = require("workbox-webpack-plugin");
-const FixStyleOnlyEntriesPlugin = require("webpack-fix-style-only-entries");
-const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const RemoveEmptyScriptsPlugin = require("webpack-remove-empty-scripts");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 
 module.exports = {
   mode: "production",
-  entry: path.join(__dirname, "src", "index.jsx"),
+  entry: path.join(__dirname, "src", "index.tsx"),
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "index_bundle.js",
@@ -16,14 +16,14 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.js|.jsx?$/,
+        test: /\.(js|jsx)$/,
         exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: ["@babel/preset-env", "@babel/preset-react"],
-          },
-        },
+        use: ["babel-loader"],
+      },
+      {
+        test: /\.(ts|tsx)$/,
+        exclude: /node_modules/,
+        use: ["ts-loader"],
       },
       {
         test: /\.css$/i,
@@ -33,11 +33,10 @@ module.exports = {
         test: /\.(png|jp(e*)g|svg|gif)$/,
         use: ["file-loader"],
       },
-      {
-        test: /\.svg$/,
-        use: ["@svgr/webpack"],
-      },
     ],
+  },
+  optimization: {
+    minimizer: [new CssMinimizerPlugin()],
   },
   plugins: [
     new HtmlWebpackPlugin({
@@ -45,40 +44,45 @@ module.exports = {
       favicon: "./public/favicon.ico",
     }),
     new MiniCssExtractPlugin(),
-    new FixStyleOnlyEntriesPlugin(),
-    new OptimizeCSSAssetsPlugin({}),
+    new RemoveEmptyScriptsPlugin(),
     new WorkboxPlugin.GenerateSW({
       clientsClaim: true,
       skipWaiting: true,
-      maximumFileSizeToCacheInBytes: 6000000,
+      maximumFileSizeToCacheInBytes: 10000000,
     }),
-    new WebpackPwaManifest({
-      prefer_related_applications: false,
-      short_name: "Price App",
-      name: "Price app for P3",
-      description: "Prices for all parts made of all available panel types",
-      lang: "en-US",
-      start_url: "/",
-      display: "standalone",
-      theme_color: "#000000",
-      background_color: "#ffffff",
-      icons: [
-        {
-          src: "./public/favicon.ico",
-          sizes: "64x64 32x32 24x24 16x16",
-          type: "image/x-icon",
-        },
-        {
-          src: "./public/logo192.png",
-          type: "image/png",
-          sizes: "192x192",
-        },
-        {
-          src: "./public/logo512.png",
-          type: "image/png",
-          sizes: "512x512",
-        },
+    new CopyPlugin({
+      patterns: [
+        { from: "public/images", to: "./" },
+        { from: "public/manifest", to: "./" },
       ],
     }),
+    // new WebpackPwaManifest({
+    //   prefer_related_applications: false,
+    //   short_name: "Price App",
+    //   name: "Price app for P3",
+    //   description: "Prices for all parts made of all available panel types",
+    //   lang: "en-US",
+    //   start_url: "/",
+    //   display: "standalone",
+    //   theme_color: "#000000",
+    //   background_color: "#ffffff",
+    //   icons: [
+    //     {
+    //       src: "./public/favicon.ico",
+    //       sizes: "64x64 32x32 24x24 16x16",
+    //       type: "image/x-icon",
+    //     },
+    //     {
+    //       src: "./public/logo192.png",
+    //       type: "image/png",
+    //       sizes: "192x192",
+    //     },
+    //     {
+    //       src: "./public/logo512.png",
+    //       type: "image/png",
+    //       sizes: "512x512",
+    //     },
+    //   ],
+    // }),
   ],
 };
