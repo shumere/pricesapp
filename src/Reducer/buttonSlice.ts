7a9df963f123a3ff1f1e9d58741ef5302a86ec1a
eXebyss <mysticlord27@gmail.com>
2021-08-22 02:58:54 +0300

    some little updates for Redux Types

--- src/Reducer/buttonSlice.ts
+++ src/Reducer/buttonSlice.ts
@@ -1,4 +1,4 @@
-import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { createSlice } from "@reduxjs/toolkit";
 
 interface ButtonState {
   materialType: string;
@@ -13,6 +13,27 @@ interface ButtonState {
   showSilencer: boolean;
   showDamper: boolean;
   showGrill: boolean;
+}
+
+interface ButtonAction1 {
+  type: string;
+  payload:
+    | "15HB21"
+    | "15HE21"
+    | "15HL21"
+    | "15HN21ABT"
+    | "15HN21PLUS"
+    | "15HS31"
+    | "15HP31"
+    | "15HR31"
+    | "15HR31ABT"
+    | "15HR31PLUS"
+    | "15HK31";
+}
+
+interface ButtonAction2 {
+  type: string;
+  payload: boolean;
 }
 
 const initialState: ButtonState = {
@@ -34,40 +55,40 @@ const buttonSlice = createSlice({
   name: "buttons",
   initialState,
   reducers: {
-    changeMaterialType: (state, action: PayloadAction<string>) => {
+    changeMaterialType: (state, action: ButtonAction1) => {
       return { ...state, materialType: action.payload };
     },
-    clickDuct: (state, action: PayloadAction<boolean>) => {
+    clickDuct: (state, action: ButtonAction2) => {
       return { ...state, showDuct: action.payload };
     },
-    clickElbow90: (state, action: PayloadAction<boolean>) => {
+    clickElbow90: (state, action: ButtonAction2) => {
       return { ...state, showElbow90: action.payload };
     },
-    clickElbow45: (state, action: PayloadAction<boolean>) => {
+    clickElbow45: (state, action: ButtonAction2) => {
       return { ...state, showElbow45: action.payload };
     },
-    clickReducer: (state, action: PayloadAction<boolean>) => {
+    clickReducer: (state, action: ButtonAction2) => {
       return { ...state, showReducer: action.payload };
     },
-    clickOffset: (state, action: PayloadAction<boolean>) => {
+    clickOffset: (state, action: ButtonAction2) => {
       return { ...state, showOffset: action.payload };
     },
-    clickTee: (state, action: PayloadAction<boolean>) => {
+    clickTee: (state, action: ButtonAction2) => {
       return { ...state, showTee: action.payload };
     },
-    clickEndCap: (state, action: PayloadAction<boolean>) => {
+    clickEndCap: (state, action: ButtonAction2) => {
       return { ...state, showEndCap: action.payload };
     },
-    clickPlenum: (state, action: PayloadAction<boolean>) => {
+    clickPlenum: (state, action: ButtonAction2) => {
       return { ...state, showPlenum: action.payload };
     },
-    clickSilencer: (state, action: PayloadAction<boolean>) => {
+    clickSilencer: (state, action: ButtonAction2) => {
       return { ...state, showSilencer: action.payload };
     },
-    clickDamper: (state, action: PayloadAction<boolean>) => {
+    clickDamper: (state, action: ButtonAction2) => {
       return { ...state, showDamper: action.payload };
     },
-    clickGrill: (state, action: PayloadAction<boolean>) => {
+    clickGrill: (state, action: ButtonAction2) => {
       return { ...state, showGrill: action.payload };
     },
   },
@@ -88,17 +109,17 @@ export const {
   clickGrill,
 } = buttonSlice.actions;
 
-export const materialType = (state: any) => state.buttons.materialType;
-export const showDuct = (state: any) => state.buttons.showDuct;
-export const showElbow90 = (state: any) => state.buttons.showElbow90;
-export const showElbow45 = (state: any) => state.buttons.showElbow45;
-export const showReducer = (state: any) => state.buttons.showReducer;
-export const showOffset = (state: any) => state.buttons.showOffset;
-export const showTee = (state: any) => state.buttons.showTee;
-export const showEndCap = (state: any) => state.buttons.showEndCap;
-export const showPlenum = (state: any) => state.buttons.showPlenum;
-export const showSilencer = (state: any) => state.buttons.showSilencer;
-export const showDamper = (state: any) => state.buttons.showDamper;
-export const showGrill = (state: any) => state.buttons.showGrill;
+export const materialType = (state) => state.buttons.materialType;
+export const showDuct = (state) => state.buttons.showDuct;
+export const showElbow90 = (state) => state.buttons.showElbow90;
+export const showElbow45 = (state) => state.buttons.showElbow45;
+export const showReducer = (state) => state.buttons.showReducer;
+export const showOffset = (state) => state.buttons.showOffset;
+export const showTee = (state) => state.buttons.showTee;
+export const showEndCap = (state) => state.buttons.showEndCap;
+export const showPlenum = (state) => state.buttons.showPlenum;
+export const showSilencer = (state) => state.buttons.showSilencer;
+export const showDamper = (state) => state.buttons.showDamper;
+export const showGrill = (state) => state.buttons.showGrill;
 
 export default buttonSlice;
